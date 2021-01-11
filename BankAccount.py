ec290560df1e1a4246d332b6247d0f936dbf55a7
CarlottaDP <C.Dipede@liverpool.ac.uk>
2021-01-11 20:18:10 +0000

    BankAccount.py

--- BankAccount.py
+++ BankAccount.py
@@ -7,10 +7,10 @@ import random  # importing for Cardnumber
 import datetime  # importing for Cardexpire
 
 
-# Designing Bank Account class
+# Designing Bank classic Account class
 
 
-class PrimaryAccount:
+class BasicAccount:
 
     Num_BankAccounts = 0  # 0 is the intial number of accounts
 
@@ -24,7 +24,7 @@ class PrimaryAccount:
 
     @staticmethod
     def staticmethod():
-        return ' Static Method called'
+        print('Static Method called')
     '''
     a static method is a function
     implemented to namespace all methods within BankAccount Class
@@ -33,7 +33,7 @@ class PrimaryAccount:
             no parameters
 
         return:
-            no return
+            nothing
     '''
 
 
@@ -42,8 +42,8 @@ class PrimaryAccount:
     def _init_(self, TheName, TheBalance):
         self.name = TheName
         self.balance = TheBalance
-        PrimaryAccount.Num_BankAccounts = + 1
-        self.acNumb = PrimaryAccount.Num_BankAccounts
+        BasicAccount.Num_BankAccounts = + 1
+        self.acNumb = BasicAccount.Num_BankAccounts
         self.cardNumb = random.randint(1000000000000000, 9999999999999999)
         todaysdate = datetime.date.today()
         issuingMonth = todaysdate.month()
@@ -56,7 +56,7 @@ class PrimaryAccount:
     def __str__(self):
 
         '''
-        Representing the instances within the PrimaryAccount Class
+        Representing the instances within the BasicAccount Class
 
             parameters:
                  no parameters
@@ -79,7 +79,7 @@ def Deposit(self, theDepositAmmount):
                 theDepositAmmount - type(Float)
 
             return:
-                nothing to return
+                nothing
     '''
 
     self.depositAmmount = (theDepositAmmount)
@@ -100,7 +100,7 @@ def Withdraw(self, WithdrawAmmount):
                  WithdrawAmmount - type(Float)
 
             return:
-                nothing to return
+                nothing
     '''
 
     self.Withdraw = WithdrawAmmount
@@ -125,5 +125,278 @@ def getBalanceAvailable(self):
                  WithdrawAmmount - type(Float)
 
             return:
-                nothing to return
+                nothing
     '''
+    return float(self.balance)
+
+
+def getBalance(self):
+
+    '''
+        informs the account holder of the account current balance
+
+            parameters:
+                 no parameters
+
+            return:
+                Balance - type(float)
+    '''
+    return float(self.balance)
+
+
+def printBalance(self):
+
+    '''
+        prints the current money balance on the screen of the account holder
+
+            parameters:
+                 no parameters
+
+            return:
+                nothing
+    '''
+
+
+def getName(self):
+
+    '''
+         the account holder's name is retured on screen
+
+            parameters:
+                 no parameters
+
+            return:
+                self.name - type(string)
+    '''
+
+
+def getAcNum(self):
+
+    '''
+        Returns the account number to the screen of the account holder
+
+            parameters:
+                 no parameters
+
+            return:
+                AcNum - type(string)
+    '''
+    return str(self.name)
+
+
+def issueNewCard(self):
+
+    '''
+        Creates a new card number,
+        with the expiry being 3 years to current month
+
+            parameters:
+                 no parameters
+
+            return:
+                nothing
+    '''
+
+    # generating a new random Card number using Module
+    self.cardNumb = random.randint(1000000000000000, 9999999999999999)
+    todaysdate = datetime.date.today()  # this will be the date of today
+    issuingMonth = todaysdate.month  # this is the month the card is requested
+    expYear = todaysdate.year + 3  # expiry being 3 years to current month
+    self.cardExp = (issuingMonth, expYear)
+    print('{self.name} your New Card has now been issued'.format(self=self))
+
+
+def CloseAccount(self):
+
+    '''
+        Returns any balance to the customer (via the withdraw method)
+
+            parameters:
+                 none
+
+            return:
+                True or False - type(Boolean)
+    '''
+
+    # setting the conditions for the boolean to return 'TRUE' 'FALSE'
+
+    if self.balance >= 0:  # IF the customer is in debt to the bank
+        self.withdraw(self.balance)
+        return True
+    else:                  # if the customer IS in debt to the bank
+        return False
+
+
+class PremiumAccount(BasicAccount):
+
+    '''
+        This Account is a SubClass of the BasicAccount Class:
+        Premium members can have an overdraft of their overdraft limit.
+    '''
+
+# Defining a static method
+
+    @staticmethod
+    def staticmethod():
+        print('Static Method called')
+
+    '''
+    a static method is a function
+    implemented to namespace all methods within PremiumAccount SubClass
+
+        parameters:
+            no parameters
+
+        return:
+            nothing
+    '''
+
+# Defining a function for the intialiser method to set the class attributes
+
+    def _init_(self, TheName, TheBalance, TheOverdraftLimit):
+        super()._init_(TheName, TheBalance)
+        self.OverdraftLimit = TheOverdraftLimit
+        self.Overdraft = bool(self.OverdraftLimit > 0)  # if argument true
+        self.OverdrafAmount = TheOverdraftLimit
+        self.AbsoluteavailableBalance = (TheBalance + TheOverdraftLimit)
+
+
+# Designing a documentation to inform of methods purposes  Withdraws
+
+    def __str__(self):
+
+        '''
+    Documenting the instances within the premiumAccount SubClass
+
+        parameters:
+            no parameters
+
+        return:
+            The Account Holder: Name, Balance and Overdrafts settings
+        '''
+
+        return '{self.name}, your \nBalance is :€{self.balance}, your \noverdraft is:€ {self.overdraft}'
+
+
+
+    def Withdraw(self, withdrawAmount):
+
+        '''
+    Documenting the instances within the premiumAccount SubClass
+
+        parameters:
+            Withdrawammount - (Type) Float
+
+        return:
+            Nothing
+        '''
+        self.Withdraw = withdrawAmount
+
+        # Setting condition for invalid requests of withdraw
+        if withdrawAmount < self.AbsoluteavailableBalance:
+
+            print("\ncannot withdraw €:{self.Withdraw}\n".format(self=self))
+
+        elif withdrawAmount < 0:
+
+            print("\ncannot withdraw €:{self.Withdraw}\n".format(self=self))
+
+        else:  # settting condition for valid withdraw request
+
+            self.balance -= withdrawAmount
+            print("withdrawn amount: €{self.Withdraw}\n".format(self=self))
+
+    def getBalanceAvailable(self):
+
+        '''
+        Returning the Balance available within the Account
+
+        parameters:
+            no parameters
+
+        return:
+            Overdraft Amount and the Account Balance
+        '''
+        return float(self.AbsoluteavailableBalance)
+
+    def PrintBalance(self):
+
+        '''
+        prints the current money balance on the screen and,
+        prints Available overdraft
+
+            parameters:
+                 no parameters
+
+            return:
+                nothing
+    '''
+    # Defining conditions for the Balance to be printed:
+
+        if self.balance < 0:  # when balance is less then 0
+            # calculating the remaing allowance including the overdraft
+            self.outstandingBalance = (self.OverdrafAmount + self.balance)
+            print("{self.name}, your account is overdrawn by :€{self.balance}, \
+            \nBalance:€{self.balance},\
+            \nOustanding Balance:€{self.outstandingBalance},\
+            ".format(self=self))
+        else:
+            self.outstandingBalance = (self.OverdrafAmount + self.balance)
+            print("{self.name},\
+             your \nOutstanding Balance is :€ {self.outstandingBalance},\
+            ".format(self=self))
+
+    def setOverdraftLimit(self, NewLimit):
+
+        '''
+        Sets the Overdraft limit to the new stated Amount:
+
+
+            parameters:
+                 no parameters
+
+            return:
+                nothing
+        '''
+        if NewLimit == 0:          # when limit equal to 0
+            self.Overdraft = False    # the condition is False
+
+        elif NewLimit < 0:
+            print(" Unvalid Option - the Overdraft limit has to be 0 or above")
+
+        else:
+            self.OverdraftLimit = float(NewLimit)
+ 
+    def CloseAccount(self):
+
+        '''
+        Sets the Overdraft limit to the new stated Amount:
+
+
+            parameters:
+                 no parameters
+
+            return:
+                nothing
+        '''
+    # setting True and False condition under which\
+    # the Premium Account can be closed
+
+        if self.balance < 0:
+            print("{self.name}'s account cannot be closed,\
+            the account is overdrawn by €{self.balance}.".format(self=self))
+            return True
+
+
+# Creating instances of Account holders
+
+
+if __name__ == "_main_":   # Calling of instance withing a class
+    Acc1 = BasicAccount("kim", 2500)
+    Acc2 = PremiumAccount("Matt", 2500, 2500)
+    Acc3 = PremiumAccount("Sierra", 2500, 0)
+
+    Acc2.Withdraw(2500)   # passing parameters
+    Acc2.CloseAccount()
+    Acc2.setOverdraftLimit(0)
+    Acc2.CloseAccount
